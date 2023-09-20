d30763ad0bc6cb29f980a19897f3f7da9a8a2235
gkspranger <greg@spranger.us>
2023-09-20 09:21:47 -0400

    fun

--- udemy-course/02-format.lisp
+++ udemy-course/02-format.lisp
@@ -1,12 +1,16 @@
+(require :asdf)
+
 (defun awesome-output (name)
   "will take a name and print it out in a pretty way"
-  (let ((num (random 100)))
-    (format t "hello ~a! your random numver is ~a.
-      i would consider this a ~a number."
+  (let ((num (+ 1 (random 100))))
+    (format t "hello ~a! your random numver is ~a.~a~
+      i would consider this a ~a number.~a"
       name
       num
+      #\newline
       (if (> num 50)
           "high"
-          "low"))))
+          "low")
+      #\newline)))
 
 (awesome-output (uiop:getenv "USER"))
